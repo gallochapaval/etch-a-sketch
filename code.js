a8b0ea563b04ca3eb08cee5c07ed68e0a2c0e84e
gallochapaval <gallochapaval@gmail.com>
2022-03-10 03:36:31 -0500

    added header and footer + prompt for custom size

--- code.js
+++ code.js
@@ -1,12 +1,14 @@
-let etchSize = 100; //H & W for the grid
+let etchSize = 16; //H & W for the grid
 
 const etchContainer = document.getElementById("etchContainer");
 const reStart = document.createElement('h1');
 const buttons = document.getElementById('buttons');
 
-reStart.classList.add('reStart','btn');
+reStart.classList.add('reStart', 'btn');
 reStart.textContent = "RESET";
-reStart.addEventListener('click', ()=> reset());
+reStart.addEventListener('click', byClick);
+reStart.addEventListener('click', () => reset());
+reStart.addEventListener('transitionend', removeTransition);
 
 buttons.appendChild(reStart);
 
@@ -14,15 +16,21 @@ function byClick(e) {
     this.classList.add('playing');
 }
 
-function reset(){
+function reset() {
     clearGrid();
+    etchSize = window.prompt("Please enter the size of your grid (a number between 4 and 100)");
+    parseInt(etchSize);
+    console.log(typeof etchSize);
+    console.log(etchSize);
+    while (isNaN(etchSize) || etchSize<4 || etchSize>100) {
+        etchSize = parseInt(window.prompt("Please enter a valid value for the size of your grid (a number between 4 and 100), or if you're bored, just close this page!"));
+    }
     setGrid(etchSize);
-    console.log("el pollo");
 }
 
 function clearGrid() {
     etchContainer.innerHTML = ''
-  }
+}
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
@@ -46,8 +54,8 @@ function changeBackground(e) {
 window.onload = () => {
     setGrid(etchSize);
 }
-
+/*
 const buttonAnimation = document.querySelectorAll(".btn");
 buttonAnimation.forEach(btn => btn.addEventListener('transitionend', removeTransition));
 buttonAnimation.forEach(btn => btn.addEventListener('click', byClick));
-
+*/
